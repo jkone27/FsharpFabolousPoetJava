0105f8cdf2eee72153cd3feeebd4e73456a28cb8
gparmigiani <gparmigiani@travix.com>
2025-01-04 00:30:13 +0000

    setup ready

--- Program.fs
+++ Program.fs
@@ -1,2 +1,46 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿open System.IO
+open Fabulous.AST
+open Fantomas.FCS
+open Fantomas.FCS.Text
+
+// using JavaPoet via IKVM .NET !yey
+// latest version from palantir does not work so using the older one here
+open com.squareup.javapoet
+
+module FSharpLoader =
+    
+    let loadFile filePath =
+        filePath
+        |> File.ReadAllText
+        |> SourceText.ofString
+        |> fun source -> Parse.parseFile false  source []
+
+module AstMapper =
+
+    // TODO: read Fable.AST/oak and output a Java using poet 
+    let fromFsharpToJava oakAst =
+        // TODO:
+        ()
+
+
+module JavaWriter =
+    open javax.lang.model.element
+    
+    let outputJava main =   
+
+        let helloWorld = 
+            TypeSpec
+                .classBuilder("HelloWorld")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(main)
+                .build()
+
+        let javaFile =  
+            JavaFile
+                .builder("com.example.helloworld", helloWorld)
+                .build()
+
+        javaFile
+
+// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
---
+++ README.md
@@ -0,0 +1,13 @@
+# F# to Java via ASTs
+
+the aim of this project is to try something similar as POC
+
+```
+.fs file ---> Fabolous AST ---> MAP ---> IVKM javaPoet ---> JAVA
+```
+
+## references
+
+* [JavaPoet](https://www.baeldung.com/java-poet)
+* [Ikvm.net](https://github.com/ikvmnet/ikvm)
+* [Fabolous AST](https://edgarfgp.github.io/Fabulous.AST/)
